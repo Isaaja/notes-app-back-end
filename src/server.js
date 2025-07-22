50e46edaa9107624b7f78f7059ad704835bc3d15
Isaaja <111202214416@mhs.dinus.ac.id>
2025-07-22 22:27:55 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
-const NotesService = require("./service/inMemory/notesService");
+const NotesService = require("./service/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
 const ClientError = require("./exceptions/ClientError");
 
