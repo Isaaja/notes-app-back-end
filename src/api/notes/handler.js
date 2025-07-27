5362bde4de5e9b5bc5d1816dd84620d8780fa17e
Isaaja <111202214416@mhs.dinus.ac.id>
2025-07-27 13:31:14 +0700

    authorization implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,36 +1,60 @@
 /* eslint no-underscore-dangle: 0 */
+const autoBind = require("auto-bind");
+const ClientError = require("../../exceptions/ClientError");
 
 class NotesHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
 
-    this.postNoteHandler = this.postNoteHandler.bind(this);
-    this.getNotesHandler = this.getNotesHandler.bind(this);
-    this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
-    this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
-    this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
+    autoBind(this);
   }
 
   async postNoteHandler(request, h) {
-    this._validator.validateNotePayload(request.payload);
-    const { title = "untitled", body, tags } = request.payload;
+    try {
+      this._validator.validateNotePayload(request.payload);
+      const { title = "untitled", body, tags } = request.payload;
+      const { id: credentialId } = request.auth.credentials;
+      const noteId = await this._service.addNote({
+        title,
+        body,
+        tags,
+        owner: credentialId,
+      });
 
-    const noteId = await this._service.addNote({ title, body, tags });
+      const response = h.response({
+        status: "success",
+        message: "Catatan berhasil ditambahkan",
+        data: {
+          noteId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
 
-    const response = h.response({
-      status: "success",
-      message: "Catatan berhasil ditambahkan",
-      data: {
-        noteId,
-      },
-    });
-    response.code(201);
-    return response;
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes();
+  async getNotesHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+    const notes = await this._service.getNotes(credentialId);
     return {
       status: "success",
       data: {
@@ -39,37 +63,104 @@ class NotesHandler {
     };
   }
 
-  async getNoteByIdHandler(request) {
-    const { id } = request.params;
-    const note = await this._service.getNoteById(id);
-    return {
-      status: "success",
-      data: {
-        note,
-      },
-    };
+  async getNoteByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._service.verifyNoteOwner(id, credentialId);
+      const note = await this._service.getNoteById(id);
+
+      return {
+        status: "success",
+        data: {
+          note,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 
-  async putNoteByIdHandler(request) {
-    this._validator.validateNotePayload(request.payload);
-    const { id } = request.params;
+  async putNoteByIdHandler(request, h) {
+    try {
+      this._validator.validateNotePayload(request.payload);
+      const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.editNoteById(id, request.payload);
 
-    await this._service.editNoteById(id, request.payload);
+      return {
+        status: "success",
+        message: "Catatan berhasil diperbarui",
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
 
-    return {
-      status: "success",
-      message: "Catatan berhasil diperbarui",
-    };
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 
-  async deleteNoteByIdHandler(request) {
-    const { id } = request.params;
-    await this._service.deleteNoteById(id);
+  async deleteNoteByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.deleteNoteById(id);
 
-    return {
-      status: "success",
-      message: "Catatan berhasil dihapus",
-    };
+      return {
+        status: "success",
+        message: "Catatan berhasil dihapus",
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 }
 
