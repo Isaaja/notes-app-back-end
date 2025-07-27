51614122a533a75dd3f8bfe0004de7c1fd036689
Isaaja <111202214416@mhs.dinus.ac.id>
2025-07-27 12:16:44 +0700

    authentication implemented

---
+++ src/api/authentications/index.js
@@ -0,0 +1,24 @@
+/*
+eslint object-curly-newline:0
+*/
+
+const AuthenticationsHandler = require("./handler");
+const routes = require("./route");
+
+module.exports = {
+  name: "authentications",
+  version: "1.0.0",
+  register: async (
+    server,
+    { authenticationsService, usersService, tokenManager, validator }
+  ) => {
+    const authenticationsHandler = new AuthenticationsHandler(
+      authenticationsService,
+      usersService,
+      tokenManager,
+      validator
+    );
+
+    server.route(routes(authenticationsHandler));
+  },
+};
